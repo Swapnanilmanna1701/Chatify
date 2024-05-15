cd1051e167978845091f0ef6f5725506658ee7c5
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-05-15 21:26:07 +0530

    dark mode setup

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,3 +1,25 @@
+/*import React from 'react';
+
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
+const ChatsPage = (props) => {
+  return (
+    <div style={{height: '100vh'}}>
+      <PrettyChatWindow
+        projectID="b7eaf7c8-e679-4961-bee7-0e529880ff49"
+        userName={props.user.username}
+        userSecret={props.user.secret}
+        styles={{ height: '100%' }}
+      />
+    </div>
+  );
+}
+export default ChatsPage;
+
+
+*/
+
+
+
 import react from "react";
 import {
   MultiChatSocket,
